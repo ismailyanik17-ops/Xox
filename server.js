e5e87c806c4d062c96d30f36e0f043d25261e05c
ismailyanik17-ops <ismailyanik17@gmail.com>
2026-03-18 10:34:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,32 +7,21 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST"] }
-});
+const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
 
-// --- MONGODB ATLAS BAĞLANTISI ---
-// <db_password> kısmını Atlas'taki şifrenle değiştirmeyi UNUTMA!
-const MONGO_URI = 'mongodb+srv://iso:xoxdeneme@cluster0.gt06c6w.mongodb.net/?appName=Cluster0'; 
+const MONGO_URI = 'mongodb+srv://iso:123456789@cluster0.gt06c6w.mongodb.net/XOX_Sultan?retryWrites=true&w=majority'; 
 
-mongoose.connect(MONGO_URI)
-    .then(() => console.log('Sultanım, MongoDB bağlantısı başarılı!'))
-    .catch(err => console.error('MongoDB hatası:', err));
+mongoose.connect(MONGO_URI).then(() => console.log('✅ MongoDB Bağlandı!'));
 
 const userSchema = new mongoose.Schema({
     username: String,
-    puan: { type: Number, default: 50 },
-    w: { type: Number, default: 0 },
-    d: { type: Number, default: 0 },
-    l: { type: Number, default: 0 }
+    puan: { type: Number, default: 50 }
 });
 const User = mongoose.model('User', userSchema);
 
 let havuzlar = { "5": [], "15": [], "30": [] };
 
 io.on('connection', (socket) => {
-    console.log('Yeni sultan bağlandı:', socket.id);
-
     socket.on('havuzaGir', (data) => {
         socket.oyuncu = data;
         if (havuzlar[data.sure] && havuzlar[data.sure].length > 0) {
@@ -42,11 +31,10 @@ io.on('connection', (socket) => {
             rakip.join(oda);
             
             const baslayan = Math.random() < 0.5 ? 'X' : 'O';
-            
             rakip.emit('macBasladi', { rakipAd: data.ad, sembol: 'X', oda: oda, baslayan: baslayan });
             socket.emit('macBasladi', { rakipAd: rakip.oyuncu.ad, sembol: 'O', oda: oda, baslayan: baslayan });
         } else {
-            if (havuzlar[data.sure]) havuzlar[data.sure].push(socket);
+            havuzlar[data.sure].push(socket);
         }
     });
 
@@ -55,16 +43,12 @@ io.on('connection', (socket) => {
     });
 
     socket.on('updateStats', async (data) => {
-        try {
-            await User.findOneAndUpdate({ username: data.ad }, data, { upsert: true });
-        } catch (e) { console.log("Stat hatası"); }
+        await User.findOneAndUpdate({ username: data.ad }, { puan: data.puan }, { upsert: true });
     });
 
     socket.on('getLeaderboard', async () => {
-        try {
-            const top = await User.find().sort({ puan: -1 }).limit(10);
-            socket.emit('leaderboardData', top);
-        } catch (e) { console.log("Liderlik hatası"); }
+        const top = await User.find().sort({ puan: -1 }).limit(10);
+        socket.emit('leaderboardData', top);
     });
 
     socket.on('disconnect', () => {
@@ -72,5 +56,4 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Sunucu ${PORT} portunda aktif.`));
+server.listen(process.env.PORT || 3000);
